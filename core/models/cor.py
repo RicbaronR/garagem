513cf119500353f579e42fefba843c8de7d14cdb
RicbaronR <ric.baron01@gmail.com>
2026-03-23 16:55:59 -0300

    feat: criacao da model Modelo

--- core/models/cor.py
+++ core/models/cor.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Cor(models.Model):
-    nome = models.CharField(max_length=50)
+    nome = models.CharField(max_length=40)
 
     def __str__(self):
-        return self.nome
+        return f"{self.id} - {self.nome}"
